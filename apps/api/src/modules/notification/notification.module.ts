13755904a5a571ac25216ccdade4d8df9810093e
digosacademy <btembong@digos.cm>
2026-08-20 07:24:32 +0100

    fix: break circular dep NotificationModule<->OrderModule<->ReferralModule with forwardRef

--- apps/api/src/modules/notification/notification.module.ts
+++ apps/api/src/modules/notification/notification.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationListener } from './notification.listener';
 import { OrderModule } from '../order/order.module';
 
 // DatabaseService is globally provided via DatabaseModule (app.module.ts)
 @Module({
-  imports: [OrderModule],
+  imports: [forwardRef(() => OrderModule)],
   providers: [NotificationService, NotificationListener],
   exports: [NotificationService],
 })
--- apps/api/src/modules/referral/referral.module.ts
+++ apps/api/src/modules/referral/referral.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ReferralService } from './referral.service';
 import { ReferralController } from './referral.controller';
 import { CreditService } from './credit.service';
@@ -8,7 +8,7 @@ import { PublicReferralController } from './public-referral.controller';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [NotificationModule],
+  imports: [forwardRef(() => NotificationModule)],
   controllers: [ReferralController, CreditController, PublicReferralController],
   providers: [ReferralService, CreditService, PublicReferralService],
   exports: [ReferralService, CreditService, PublicReferralService],
